db652834e86c8e00e2427464053b4e45ff808d9a
Johannes Nussbaum <johannes.nussbaum@dasch.swiss>
2024-12-16 20:15:23 +0100

    fix table structure

--- app/src/components/ComparisonTable.tsx
+++ app/src/components/ComparisonTable.tsx
@@ -10,37 +10,40 @@ const ComparisonTable = () => {
     const two_wheeler_info = getTravelInfo("Bern", "Zürich", TravelMode.TWO_WHEELER);
 
     return <table>
-        <th>Resultate</th>
-        <tr>
-            <td>Verkehrsmittel</td>
-            <td>Distanz</td>
-            <td>Zeit</td>
-        </tr>
-        <tr>
-            <td>Auto</td>
-            <td>{car_info.formattedDistance}</td>
-            <td>{car_info.formattedTime}</td>
-        </tr>
-        <tr>
-            <td>Öffentliche Verkehrsmittel</td>
-            <td>{public_transport_info.formattedDistance}</td>
-            <td>{public_transport_info.formattedTime}</td>
-        </tr>
-        <tr>
-            <td>Velo</td>
-            <td>{bike_info.formattedDistance}</td>
-            <td>{bike_info.formattedTime}</td>
-        </tr>
-        <tr>
-            <td>Fussgänger</td>
-            <td>{walk_info.formattedDistance}</td>
-            <td>{walk_info.formattedTime}</td>
-        </tr>
-        <tr>
-            <td>Motorrad</td>
-            <td>{two_wheeler_info.formattedDistance}</td>
-            <td>{two_wheeler_info.formattedTime}</td>
-        </tr>
+        <thead>
+            <tr>
+                <th>Verkehrsmittel</th>
+                <th>Distanz</th>
+                <th>Zeit</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Auto</td>
+                <td>{car_info.formattedDistance}</td>
+                <td>{car_info.formattedTime}</td>
+            </tr>
+            <tr>
+                <td>Öffentliche Verkehrsmittel</td>
+                <td>{public_transport_info.formattedDistance}</td>
+                <td>{public_transport_info.formattedTime}</td>
+            </tr>
+            <tr>
+                <td>Velo</td>
+                <td>{bike_info.formattedDistance}</td>
+                <td>{bike_info.formattedTime}</td>
+            </tr>
+            <tr>
+                <td>Fussgänger</td>
+                <td>{walk_info.formattedDistance}</td>
+                <td>{walk_info.formattedTime}</td>
+            </tr>
+            <tr>
+                <td>Motorrad</td>
+                <td>{two_wheeler_info.formattedDistance}</td>
+                <td>{two_wheeler_info.formattedTime}</td>
+            </tr>
+        </tbody>
     </table>;
 };
 
