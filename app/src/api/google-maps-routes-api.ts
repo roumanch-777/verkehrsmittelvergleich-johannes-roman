d75b5ab6f68214a41c47dd730b08c9418e6a8a1a
Johannes Nussbaum <johannes.nussbaum@dasch.swiss>
2025-01-15 22:50:25 +0100

    improve time/distance formatting

--- app/src/api/google-maps-routes-api.ts
+++ app/src/api/google-maps-routes-api.ts
@@ -163,7 +163,18 @@ function computeDistanceString(meters_total: number): string {
     } else {
         const kilometers = Math.floor(meters_total / 1000);
         const meters = meters_total % 1000;
-        formattedDistance = `${kilometers} km ${meters} m`;
+        if(kilometers < 100) {
+            const decimal = Math.round(meters / 100);
+            if(decimal === 0) {
+                formattedDistance = `${kilometers} km`
+            } else if(decimal === 10) {
+                formattedDistance = `${kilometers + 1} km`
+            } else {
+                formattedDistance = `${kilometers}.${decimal} km`
+            }
+        } else {
+            formattedDistance = `${kilometers} km`;
+        }
     }
     return formattedDistance
 }
@@ -174,13 +185,18 @@ function computeTimeString(seconds_total: number): string {
     if (seconds_total < 60) {
         formattedTime = `${seconds_total} s`;
     } else if (seconds_total < 3600) {
-        const minutes = Math.floor(seconds_total / 60);
+        let minutes = Math.floor(seconds_total / 60);
         const seconds = seconds_total % 60;
-        formattedTime = `${minutes} min ${seconds} s`;
+        if(seconds > 30) {minutes += 1};
+        formattedTime = `${minutes} min`;
     } else {
         const hours = Math.floor(seconds_total / 3600);
         const minutes = Math.floor((seconds_total / 60) - (hours * 60));
-        formattedTime = `${hours} h ${minutes} min`;
+        if(minutes !== 0) {
+            formattedTime = `${hours} h ${minutes} min`
+        } else {
+            formattedTime = `${hours} h`
+        }
     }
     return formattedTime
 }
