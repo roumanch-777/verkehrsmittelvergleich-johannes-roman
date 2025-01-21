a0efc9f0fd5769de363d844f72b1dc459dd5bc1e
Johannes Nussbaum <johannes.nussbaum@dasch.swiss>
2025-01-21 19:03:52 +0100

    working version, but cannot parse data

--- app/src/components/Diagram.tsx
+++ app/src/components/Diagram.tsx
@@ -2,7 +2,14 @@ import { Bar } from 'react-chartjs-2';
 import { AllTravelData } from '../api/google-maps-routes-api';
 
 
-interface DiagramData {}
+interface DiagramData {
+    labels: string[];
+    datasets: {
+        label: string;
+        data: number[];
+        backgroundColor: string[];
+    }[];
+}
 
 
 interface DiagramDataProps {
@@ -14,21 +21,27 @@ const Diagram: React.FC<DiagramDataProps> = ({ diagramData }) => {
     if(!diagramData) {
         return null;
     }
-    // {
-    //     labels: ["Januar", "Februar", "März", "April", "Mai"],
-    //     datasets: [
-    //       {
-    //         label: "Umsatz",
-    //         data: [3000, 2000, 4000, 5000, 6000],
-    //         backgroundColor: ["#FF6384", "#36A2EB", "#FFCE56", "#4BC0C0", "#9966FF"],
-    //       },
-    //     ],
-    //   }
+    const formattedDiagramData: DiagramData = {
+        labels: ["Auto", "Fahrrad", "Zu Fuß", "Motorrad", "Öffentliche Verkehrsmittel"],
+        datasets: [
+            {
+                label: "Dauer",
+                data: [
+                    diagramData.drive?.formattedTime ? parseInt(diagramData.drive.formattedTime) : 0,
+                    diagramData.bicycle?.formattedTime ? parseInt(diagramData.bicycle.formattedTime) : 0,
+                    diagramData.walk?.formattedTime ? parseInt(diagramData.walk.formattedTime) : 0,
+                    diagramData.two_wheeler?.formattedTime ? parseInt(diagramData.two_wheeler.formattedTime) : 0,
+                    diagramData.transit?.formattedTime ? parseInt(diagramData.transit.formattedTime) : 0,
+                ],
+                backgroundColor: ["#FF6384", "#36A2EB", "#FFCE56", "#4BC0C0", "#9966FF"],
+            },
+        ],
+    };
     return (
         <div className="chart-container">
             <h2 style={{ textAlign: "center" }}>Balkendiagramm</h2>
             <Bar 
-                data={diagramData} 
+                data={formattedDiagramData} 
                 options={{
                     plugins: {
                     title: {
